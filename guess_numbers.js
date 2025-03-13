8ec0dcdd97378dc70f66dfc7683c95ecf9bd5ba1
JanaB146 <jana.bogner@gmx.de>
2025-03-13 15:58:49 +0100

    tries added

--- guess_numbers.js
+++ guess_numbers.js
@@ -1,12 +1,11 @@
 let randomNumber, upperLimit = 100, guessedNumber;
-let tries = 5;
+let tries = 4;
 let gameOn = true;
 
 function startGuessNumbers() {
     // Der Computer generiert eine Zufallszahl
     randomNumber = Math.random() * 90 + 10;
     randomNumber = Math.ceil(randomNumber);
-    alert(randomNumber);
     // Beim Klicken auf das Fenster erscheint ein anderer Text
     document.getElementById("game-text").innerText = "Rate eine Zahl zwischen 0 und 100";
 }
@@ -15,7 +14,8 @@ function checkInput() {
     if (randomNumber == null) {
         return;
     }
-    while (gameOn && tries >= 0) {
+    while (gameOn && tries > 0) {
+        document.getElementById("versuche").innerText=`Du hast noch ${tries} Versuche.`;
         guessedNumber = document.getElementById("input").value;
         document.getElementById("ueberschrift").innerText = guessedNumber;
         if (guessedNumber >= 0 && guessedNumber <= 100) {
@@ -32,11 +32,13 @@ function checkInput() {
         } else {
             document.getElementById("game-text").innerText = "Bitte bleib zwischen 0 und 100.";
         }
-        
+        document.getElementById("input").value='';
     return false;
+    
 }
     if (guessedNumber != randomNumber) {
         document.getElementById("game-text").innerHTML = `Du hast alle Versuche verbraucht und dennoch verloren. Die Zufallsnummer war ${randomNumber}.`;
+        document.getElementById("versuche").innerText=`Du hast noch 0 Versuche.`;
         return false;
     }
     return false;
